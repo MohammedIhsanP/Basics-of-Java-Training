d4d841ba66da670b00f08865f10280f2a33ffa32
MohammedIhsanP <ihsudarulaman@gmail.com>
2022-07-26 21:31:21 +0530

    changes in palindrome or not added

--- src/palindrome.java
+++ src/palindrome.java
@@ -2,12 +2,35 @@ import java.util.Scanner;
 
 public class palindrome {
 
+    static int reverseNumber(int n){
+
+        int rev_n = 0;
+
+        while(n>0){
+            int digit = n%10;
+            rev_n = (rev_n*10) + digit;
+            n = n/10;
+        }
+        return rev_n;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number :");
-        int numb = sc.nextInt();
+        while (true){
+            System.out.println("Enter the number :");
+            int numb = sc.nextInt();
+
+            int rev_numb = reverseNumber(numb);
+
+            if (rev_numb==numb){
+                System.out.println("The number is Palindrome");
+            }
+            else {
+                System.out.println("The number is not Palindrome");
+            }
 
 
     }
+}
 }
